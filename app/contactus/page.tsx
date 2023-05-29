e8a27d85f1acf45c25969ad514c6e93838c6ab4c
Bryant Wyatt <bryant.wyatt@gmail.com>
2023-05-29 11:09:32 -0400

    Update contactus page & package.json - Contact Us now links to other contact pages - Updated prettier for es-lint prettier

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -1,5 +1,48 @@
+import Link from 'next/link'
+
 const Page = () => {
-  return <h1>Under Construction</h1>
+  const pages = [
+    {
+      href: 'contactus/search',
+      text: 'Search',
+    },
+    {
+      href: 'contactus/create',
+      text: 'Create Contact',
+    },
+    {
+      href: 'contactus/view',
+      text: 'View Contacts',
+    },
+  ]
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-full max-w-xs">
+        <h1 className="pt-8">Contact Us Home</h1>
+        <div className="flex flex-col">
+          <ul>
+            {pages.map((page) => {
+              return (
+                <li key={page.text}>
+                  <Link
+                    href={page.href}
+                    className="
+                    dark:text-green-500
+                    no-underline
+                    hover:underline-offset-8
+                  "
+                  >
+                    {page.text}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Page
