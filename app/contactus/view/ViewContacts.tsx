d6600230f39d90b93446d04b27ffe72309ef49d3
Bryant Wyatt <bryant.wyatt@gmail.com>
2023-06-10 22:32:53 -0400

    Add Redux-Toolkit - Update View Contacts to use redux

--- app/contactus/view/ViewContacts.tsx
+++ app/contactus/view/ViewContacts.tsx
@@ -1,11 +1,34 @@
 'use client';
 
+import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { fetchContacts, selectContacts } from "@/redux/slices/contactUsSlice";
+import { useEffect, useState } from "react";
+
+
 const ViewContacts = () => {
+    const dispatch = useAppDispatch();
+    const contacts = useAppSelector(selectContacts);
+
+    useEffect(() => {
+        const fetchData = () => {
+            dispatch(fetchContacts());
+        }
+        fetchData();
+    },[])
+
     return (
         <div className="flex justify-center">
             <div className="w-full max-w-xs">
-                <h1 className="flex justify-center pt-8">View Contacts</h1>
-                <h1 className="flex justify-center pt-8">Under Constrution</h1>
+                <h1 className="flex justify-center pt-8 pb-4 underline">View Contacts</h1>
+                <div className="flex flex-col space-y-4 justify-center">
+                {contacts.map((contact) => {
+                    return (
+                        <div key={`contact-${contact.firstName}`} className="bg-white rounded outline-none py-4 px-4">
+                            <p className="text-black font-bold">{contact.firstName} {contact.lastName}</p>
+                        </div>
+                        )
+                })}
+                </div>
             </div>
         </div>)
 }
---
+++ redux/slices/contactUsSlice.ts
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { RootState } from '../store';
+import ContactUsService from "@/services/ContactUsService";
+import FomatUtils from "@/utils/FormatUtils";
+
+interface ContactsState {
+    loading: string;
+    contacts: any[]
+}
+
+const initialState: ContactsState = {
+    loading: 'idle',
+    contacts: []
+}
+
+export const fetchContacts = createAsyncThunk(`${window.location.origin}/contactus/view/api`, async () => {
+    const response = await ContactUsService.getContacts();
+    const formattedResponse = FomatUtils.formatContacts(response);
+    return formattedResponse;
+})
+
+export const contactUsSlice = createSlice({
+    name: "contactUs",
+    initialState,
+    reducers: {
+        loadingContacts(state, action) {
+            if (state.loading === 'idle') {
+                state.loading = 'pending'
+            }
+        },
+        contactsReceived(state, action) {
+            if (state.loading === 'idle') {
+                state.contacts = action.payload
+            }
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchContacts.fulfilled, (state, action) => {
+            state.contacts = action.payload;
+        })
+    }
+});
+
+export const {loadingContacts, contactsReceived } = contactUsSlice.actions
+
+export const selectContacts = (state: RootState) => state.contactUs.contacts
+export default contactUsSlice.reducer;
